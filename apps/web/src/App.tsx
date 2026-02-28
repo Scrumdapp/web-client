da95d466e8385b9618c04951f018d04774a34902
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-02-28 19:02:18 +0100

    Move to turborepo

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import * as React from "react";
+import * as process from "node:process";
+
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="h-screen">
+                <h1 className="">Hallo van Scrumdapp</h1>
+            </div>
+        )
+    }
+}
