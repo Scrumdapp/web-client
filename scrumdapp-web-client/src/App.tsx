eb827236e4f001e36a47461908e54b21f1227b62
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-03-02 12:14:36 +0100

    The basics have been setup for transferring the components.

--- scrumdapp-web-client/src/App.tsx
+++ scrumdapp-web-client/src/App.tsx
@@ -1,2 +1,14 @@
-import './App.css'
+import './index.css'
+import './stylesheet.css'
+import "tailwindcss";
+import * as React from "react";
 
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="flex flex-col h-screen justify-center items-center text-center bg-fg3">
+            <button className="btn">Hallo</button>
+            </div>
+        )
+    }
+}
