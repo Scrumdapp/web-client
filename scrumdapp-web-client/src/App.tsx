4ba6ef5a902729286adc51e03a730afbd610ddf1
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-03-03 13:53:30 +0100

    I have tried to copy the button from the current Scrumdapp and translated it to the new Scrumdapp. V1

--- scrumdapp-web-client/src/App.tsx
+++ scrumdapp-web-client/src/App.tsx
@@ -7,7 +7,7 @@ export default class App extends React.Component<{}, {}> {
     render() {
         return (
             <div className="flex flex-col h-screen justify-center items-center text-center bg-fg3">
-            <button className="btn">Hallo</button>
+                <button className="btn">Scrumdapp</button>
             </div>
         )
     }
