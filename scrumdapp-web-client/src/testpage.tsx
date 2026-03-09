591e193f25eef359dbef7d06de571171f49b9da9
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-03-09 12:08:31 +0100

    Moved files from App.tsx to testapp.tsx

---
+++ scrumdapp-web-client/src/testpage.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export default class Test extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="flex flex-col h-screen justify-center items-center text-center bg-fg3">
+                    <textarea className="write-section" />
+                    <button className="btn btn-main border m-2">Scrumdapp</button>
+                </div>
+
+            </div>
+
+
+
+        )
+    }
+}
