ad6ba67a92a63d1a0942a4183010cbacfec05deb
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-03-12 12:10:23 +0100

    Made the Header floating like the Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 export default function Header() {
 
     const [isOpen, setIsOpen] = useState(false);
 
-        return (<div className="bg-(--bg-primary) border-b border-(--color-primary)">
+    const links = [
+        { to: "/public", label: "Home" },
+        { to: "/groups", label: "Groups" },
+    ];
 
-        <nav>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        return (
+    <div className="bg-(--bg-primary) border-(--color-primary)">
+        <nav className="pt-4 pb-7 px-2 flex flex-col">
+            <div className="mx-auto px-4 lg:px-12 rounded-2xl bg-bg shadow-xl border footer w-11/12">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
 
                     <div className="shrink-0 text-(--color-primary) text-2xl font-bold">
                         <a href="/public">Scrumdapp</a>
                     </div>
 
-
                     <div className="hidden md:flex space-x-6 text-black font-medium">
-                        <a href="/public" className="btn btn-main ">Home</a>
-                        <a href="/groups" className="btn btn-main">Groups</a>
+                        {links.map(link => (
+                            <Link key={link.to} to={link.to} className="btn">
+                                {link.label}
+                            </Link>
+                        ))}
                     </div>
-
 
                     <div className="hidden md:block">
                         <a href="/login"
@@ -27,7 +36,6 @@ export default function Header() {
                             Profile
                         </a>
                     </div>
-
 
                     <div className="md:hidden">
                         <button
@@ -50,7 +58,7 @@ export default function Header() {
                 </div>
             </div>
 
-
+            {/*WORK IN PROGRESS, NEED TO LOOK AT THIS AT A LATER TIME, NOT AS IMPORTANT RIGHT NOW*/}
             <div id="mobile-menu" className="md:hidden hidden px-4 pb-4 space-y-2 text-white font-medium">
                 <a href="/public" className="block hover:text-yellow-300 transition">Home</a>
                 <a href="/about" className="block hover:text-yellow-300 transition">About</a>
@@ -59,6 +67,7 @@ export default function Header() {
                 <a href="/support" className="block hover:text-yellow-300 transition">Support</a>
                 <a href="/login" className="block bg-white text-indigo-700 text-center px-4 py-2 rounded-xl hover:bg-yellow-300 transition-all font-semibold mt-2">
                     Log In</a>
+            </div>
             </div>
         </nav>
         </div>)
