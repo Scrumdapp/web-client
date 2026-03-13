bbc22c0e0c605fa1d3fa8466c8dc1601b65a8a5e
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-03-13 12:20:14 +0100

    Fix compile error with useUser throwing an error

---
+++ src/js/context/user/useUser.ts
@@ -0,0 +1,11 @@
+import type {User} from "../../models/user.ts";
+import {useContext} from "react";
+import {userContext} from "./userContext.ts";
+
+export function useUser(): User {
+    const userCtx = useContext(userContext)
+    if (!userCtx?.user) {
+        console.error("Warning! User was not set, is it provided by the UserProvider?")
+    }
+    return userCtx?.user
+}
--- src/js/context/user/userContext.ts
+++ src/js/context/user/userContext.ts
@@ -3,7 +3,6 @@ import type {User} from "../../models/user.ts";
 
 export class UserContextState {
     user: User | null
-    loading: boolean
 }
 
 export const userContext = createContext<UserContextState | null>(null)
