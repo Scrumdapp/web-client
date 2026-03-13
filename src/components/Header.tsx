4d1eb20528e8d4645a332e632b4a58822e2d1525
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-03-13 10:22:02 +0100

    Changes for PR8

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,19 +12,19 @@ export default function Header() {
     ];
 
         return (
-    <div className="bg-(--bg-primary) border-(--color-primary)">
+    <div className="bg-bg">
         <nav className="pt-4 pb-7 px-2 flex flex-col">
             <div className="mx-auto px-4 lg:px-12 rounded-2xl bg-bg shadow-xl border footer w-11/12">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
 
-                    <div className="shrink-0 text-(--color-primary) text-2xl font-bold">
+                    <div className="shrink-0 text-fg text-2xl font-bold">
                         <a href="/public">Scrumdapp</a>
                     </div>
 
                     <div className="hidden md:flex space-x-6 text-black font-medium">
-                        {links.map(link => (
-                            <Link key={link.to} to={link.to} className="btn">
+                        {links.map((link, i) => (
+                            <Link key={i} to={link.to} className="btn">
                                 {link.label}
                             </Link>
                         ))}
@@ -32,7 +32,7 @@ export default function Header() {
 
                     <div className="hidden md:block">
                         <a href="/login"
-                           className="btn border btn-extra-actions">
+                           className="btn border btn-secondary">
                             Profile
                         </a>
                     </div>
@@ -58,7 +58,7 @@ export default function Header() {
                 </div>
             </div>
 
-            {/*WORK IN PROGRESS, NEED TO LOOK AT THIS AT A LATER TIME, NOT AS IMPORTANT RIGHT NOW*/}
+            {/*WORK IN PROGRESS, NEED TO LOOK AT THIS AT A LATER TIME, NOT AS IMPORTANT AS OF RIGHT NOW*/}
             <div id="mobile-menu" className="md:hidden hidden px-4 pb-4 space-y-2 text-white font-medium">
                 <a href="/public" className="block hover:text-yellow-300 transition">Home</a>
                 <a href="/about" className="block hover:text-yellow-300 transition">About</a>
