31846baadd98b0d4edac78332e206bca8b7af391
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-03-13 15:56:26 +0100

    Improve loading widget

--- msw/handlers/userHandlers.ts
+++ msw/handlers/userHandlers.ts
@@ -34,9 +34,9 @@ export const userData: User[] = [
 
 export const userHandlers = [
     http.get("/api/users/@me", async ({}) => {
-        await new Promise((res) => {
-            setTimeout(() => res(), 2000)
-        })
+        // await new Promise((res) => {
+        //     setTimeout(() => res(), 2000)
+        // })
         return HttpResponse.json(userData[0])
     }),
 ]
