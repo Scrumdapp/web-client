52b13b8f0dfb837df3553b64798f6bbc6452ea4c
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-03-16 12:31:37 +0100

    Small stiling for error screen

--- src/components/generic/ErrorScreen.tsx
+++ src/components/generic/ErrorScreen.tsx
@@ -2,8 +2,8 @@ import type {ApiError} from "../../js/hooks/api/apiError.ts";
 
 export function ErrorScreen({error}: {error: ApiError}) {
     return (
-        <div>
-            <h2>Error: {error.status}</h2>
+        <div className="center fade-in">
+            <h2>Error: <strong>{error.status}</strong></h2>
             <p>{error.message}</p>
             <p>{error.detail}</p>
         </div>
