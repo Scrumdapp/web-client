b0800c3b8afa7b5be2fdf72f1a01784bfcb46d1b
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-03-17 16:58:55 +0100

    Loaded Luc's components.

--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import Checkin from "../components/Checkin.tsx";
+import Checkin from "../components/checkins/Checkin.tsx";
 
 export default class Test extends React.Component {
     render() {
         return (
             <div>
-                <div className="flex flex-col h-screen justify-center items-center text-center bg-fg3">
-                    <Checkin />
+                <div className="flex flex-col h-screen justify-center items-center text-center bg-bg">
+                    <Checkin groupId={1} />
                 </div>
             </div>
 
