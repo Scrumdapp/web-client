2dd7d2e35464e43f92dbf68f0c5a60e260cc3ca8
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-03-17 09:41:51 +0100

    Updated from main/fixed page not loading

--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import Checkin from "../components/Check-in.tsx";
+import Checkin from "../components/Checkin.tsx";
 
 export default class Test extends React.Component {
     render() {
         return (
             <div>
                 <div className="flex flex-col h-screen justify-center items-center text-center bg-fg3">
-                    <Checkin/>
+                    <Checkin />
                 </div>
             </div>
 
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage.tsx";
 import About from "../pages/About.tsx";
 import Privacy from "../pages/Privacy.tsx";
+import Testpage from "../pages/testpage.tsx";
 
 // ^^^ Add new pages here ^^^
 
@@ -13,6 +14,7 @@ export default function AppRouter() {
             <Route path="/" element={<MainPage />} />
             <Route path="/about" element={<About />} />
             <Route path="/privacy" element={<Privacy />} />
+            <Route path="/test" element={<Testpage />} />
         </Routes>
     );
 }
