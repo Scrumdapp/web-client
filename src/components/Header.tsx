c94bebdbd125623ae8429c93ab779f171efad70a
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-03-17 14:38:54 +0100

    smoll header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
         <nav className="pt-4 pb-7 px-2 flex flex-col">
             <div className="mx-auto px-4 lg:px-12 rounded-2xl bg-bg shadow-xl border footer w-11/12">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16 items-center">
+                <div className="flex justify-between h-10 items-center">
 
                     <div className="shrink-0 text-fg text-2xl font-bold">
                         <a href="/public">Scrumdapp</a>
