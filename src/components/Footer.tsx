5f1c6641ba72ab940c5c910bc13d54bb54b68449
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-03-17 14:46:29 +0100

    smoll footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,10 +17,10 @@ export default function Footer() {
 
     return (
     <footer className="pt-16 pb-7 px-2 flex flex-col">
-        <div className="mx-auto px-4 lg:px-12 p-12 rounded-2xl bg-bg shadow-xl border footer w-11/12">
-            <div className="flex flex-row items-center justify-between pb-8">
+        <div className="mx-auto px-4 lg:px-12 py-4 rounded-2xl bg-bg shadow-xl border w-11/12">
+            <div className="flex flex-row items-center justify-between pb-4">
                 {links.map(link => (
-                    <Link key={link.to} to={link.to} className="btn">
+                    <Link key={link.to} to={link.to} className="btn min-w-20 max-w-20 ">
                         {link.label}
                     </Link>
                 ))}
@@ -28,7 +28,7 @@ export default function Footer() {
 
             <div className="bg-fg h-0.5 rounded-full"></div>
 
-            <div className="flex flex-row items-center justify-between pt-8">
+            <div className="flex flex-row items-center justify-between pt-4">
                 <h1 className="text-(--color-primary)">© 2026 Scrumdapp. All rights reserved</h1>
                 <div className="flex items-center gap-4">
                     <a href="https://x.com"
