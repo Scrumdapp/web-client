c4ff95ed0b0109f6969bd36d58d820eb87ace559
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-03-23 10:33:41 +0100

    Table content fitted correctly

--- src/components/checkins/Checkin.tsx
+++ src/components/checkins/Checkin.tsx
@@ -22,26 +22,26 @@ function Checkin({ groupId }: {groupId: number, userId: number} ) {
                     checkup_stars: true
                 }]} >
                     {checkin => (
-                <table className="text-fg w-full table-fixed">
+                <table className="text-fg w-full">
                     <thead>
                     <tr>
-                        <th className="py-3">Name</th>
-                        <th className="py-3">Attendance</th>
+                        <th className="py-3 text-left pl-2">Name</th>
+                        <th className="py-3 text-left pl-2">Attendance</th>
                         <th className="py-3">Check-in</th>
                         <th className="py-3">Check-up</th>
-                        <th className="py-3">More Actions</th>
+                        <th className="py-3 text-right pr-2">Actions</th>
                     </tr>
                     </thead>
                     <tbody>
                     {checkin.map(item => (
                         <tr key={item.user_id}>
-                            <td className="py-3">{item.first_name} {item.last_name}</td>
-                            <td className={`py-3 ${getAttendanceColor(formatPresence(item.presence ?? "---"))}`}>
+                            <td className="py-3 text-left pl-2">{item.first_name} {item.last_name}</td>
+                            <td className={`py-3 text-left pl-2 ${getAttendanceColor(formatPresence(item.presence ?? "---"))}`}>
                                 {formatPresence(item.presence ?? "---")}
                             </td>
                             <td className={getStarsColor(item.checkin_stars)}><Stars amount={item.checkin_stars}  /></td>
                             <td className={getStarsColor(item.checkup_stars)}><Stars amount={item.checkup_stars}/></td>
-                            <td className="flex justify-center">
+                            <td className="text-right pr-2">
                                 <button className="btn btn-secondary border">More</button>
                             </td>
                         </tr>))}
