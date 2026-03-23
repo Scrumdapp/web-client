c625bb7eac1a856cdf43aebfef798c82b9425667
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-03-23 12:26:16 +0100

    fix header changing sizes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,8 +14,8 @@ export default function Header() {
         return (
     <div className="bg-bg">
         <nav className="pt-4 pb-7 px-2 flex flex-col">
-            <div className="mx-auto py-3 px-4 lg:px-12 rounded-2xl bg-bg shadow-xl border footer w-11/12">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto p-2 px-3 rounded-2xl bg-bg shadow-xl border footer w-11/12">
+                <div className="mx-auto">
                 <div className="flex justify-between h-10 items-center">
 
                     <div className="shrink-0 text-fg text-2xl font-bold max-w-20">
