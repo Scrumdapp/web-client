ea5269a3fc3bc2103992cb8c5f044774a5c5973a
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-03-25 13:24:46 +0100

    Add 404 page for groups

--- src/router/GroupRouter.tsx
+++ src/router/GroupRouter.tsx
@@ -1,11 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import {GroupTestPage} from "../pages/groups/GroupTestPage.tsx";
+import {ErrorScreen} from "../components/generic/ErrorScreen.tsx";
+import {ApiError} from "../js/hooks/api/apiError.ts";
 
 export function GroupRouter() {
 
     return (
         <Routes>
             <Route index element={<GroupTestPage/>} />
+            <Route path="*" element={<ErrorScreen error={new ApiError(404, "Page Not Found")} />} />
         </Routes>
     )
 }
