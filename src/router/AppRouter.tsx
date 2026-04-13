0bce53861fe8a8b99d7400729e307c0ca7c6295a
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-04-13 16:30:37 +0200

    Added Error Page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage.tsx";
 import About from "../pages/About.tsx";
 import Privacy from "../pages/Privacy.tsx";
+import ErrorPage from "../pages/ErrorPage.tsx";
 import {GroupLayout} from "../components/groups/GroupLayout.tsx";
 // ^^^ Add new pages here ^^^
 
@@ -14,6 +15,7 @@ export default function AppRouter() {
             <Route path="/groups/:groupId/*" element={<GroupLayout />}/>
             <Route path="/about" element={<About />} />
             <Route path="/privacy" element={<Privacy />} />
+            <Route path="*" element={<ErrorPage />} />
         </Routes>
     );
 }
