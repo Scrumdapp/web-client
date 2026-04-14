36bc1be13a0c781984ffc4cd83ffa267c7330fc4
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-04-14 14:01:17 +0200

    Working Modal

--- src/components/generic/Modal.tsx
+++ src/components/generic/Modal.tsx
@@ -8,12 +8,13 @@ type ModalProps = {
 }
 
 export default function Modal({ onClose, title, children, actions }: ModalProps) {
+
     return (
         <div className="flex fixed inset-0 backdrop-blur-lg justify-center items-center" onClick={onClose}>
             <div className="bg-bg_h rounded-lg p-6 w-96 border" onClick={(e) => e.stopPropagation()}>
                 <h2 className="text-fg mb-4">{title}</h2>
                 {children}
-                <div className="flex gap-2 mt-4 justify-end">
+                <div className="flex gap-2 mt-4 justify-between">
                     {actions}
                 </div>
             </div>
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,23 +1,35 @@
 import Modal from "../components/generic/Modal.tsx";
+import {useState} from "react";
+import {faCheck, faPlus, faRotateLeft} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function Test() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-
         <div>
-        <Modal
-            title="New Group"
-            onClose={() => setOpen(false)}
-            actions={
-                <>
-                    <button onClick={() => setOpen(false)} className="btn border">Cancel</button>
-                    <button onClick={() => setOpen(false)} className="btn border">Submit</button>
-                </>
-            }
-        >
-
-            <input className="w-full p-2 border rounded" />
-            
-        </Modal>
+            <div>
+                <button className="btn border max-h-fit"
+                        onClick={() => setIsModalOpen(true)}>
+                    New Group <FontAwesomeIcon icon={faPlus} className="icon" /></button>
+            </div>
+            {isModalOpen && (
+                <Modal
+                    title="New Group"
+                    onClose={() => setIsModalOpen(true)}
+                    actions={
+                        <>
+                            <button onClick={() => setIsModalOpen(false)} className="btn border"><FontAwesomeIcon icon={faRotateLeft} className="text-gray icon" />Cancel</button>
+                            <button onClick={() => setIsModalOpen(false)} className="btn border"><FontAwesomeIcon icon={faCheck} className="text-blue icon" />Submit</button>
+                        </>
+                    }
+                >
+                    <input
+                        type="text"
+                        placeholder="Group Name"
+                        className="w-full p-2 rounded-lg mb-4 outline-none border"
+                    />
+                </Modal>)}
         </div>
     )
 }
