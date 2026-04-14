c5372017f1657cd1767fcaf06bc2a99b6c711cac
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-04-14 11:43:57 +0200

    Added confirm button and the variable "onConfirm"

--- src/components/checkins/Checkin.tsx
+++ src/components/checkins/Checkin.tsx
@@ -5,22 +5,23 @@ import {getStarsColor, getAttendanceColor} from "../../js/utils/colorUtils.ts";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPencil} from "@fortawesome/free-solid-svg-icons"
 import {faAdd} from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import {getformatPresence} from "../../js/utils/colorUtils.ts";
-import { useState} from "react";
+import {useState} from "react";
 
-function Checkin({ groupId, date }: { groupId: number, date: string }) {
+function Checkin({groupId, date}: { groupId: number, date: string }) {
     const GetGroupCheckinsComponent = useApiComponent(ScrumdappApi.getGroupCheckinsWithUsers())
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return <div className="card w-fit space-x-5">
         <div className="flex justify-between w-full">
-        <h2 className="p-2 text-left">
-            Check-in voor: <span className="text-fg">{date}</span>
-        </h2>
+            <h2 className="p-2 text-left">
+                Check-in voor: <span className="text-fg">{date}</span>
+            </h2>
             <button className="btn border max-h-fit"
-            onClick={() => setIsModalOpen(true)}>
-            <FontAwesomeIcon icon={faAdd} className="icon text-blue" />Create Checkpoint</button>
+                    onClick={() => setIsModalOpen(true)}>
+                <FontAwesomeIcon icon={faAdd} className="icon text-blue"/>Create Checkpoint
+            </button>
         </div>
         <GetGroupCheckinsComponent input={[groupId, date, {
             presence: true,
@@ -44,9 +45,9 @@ function Checkin({ groupId, date }: { groupId: number, date: string }) {
                                 {getformatPresence(item.presence ?? "---")}
                             </td>
                             <td className={`p-3 ${getStarsColor(item.checkin_stars)}`}>
-                            <div className="flex justify-center items-center">
-                                <Stars amount={item.checkin_stars}/>
-                            </div>
+                                <div className="flex justify-center items-center">
+                                    <Stars amount={item.checkin_stars}/>
+                                </div>
                             </td>
                         </tr>))}
                     </tbody>
@@ -55,27 +56,37 @@ function Checkin({ groupId, date }: { groupId: number, date: string }) {
         </GetGroupCheckinsComponent>
         <div className="align-center horizontal gap-3 mt-2">
             <div className="flex-1"></div>
-            <Link to={`/groups/${groupId}/edit?date=${date}`} className="btn border m-auto mx-2"><FontAwesomeIcon icon={faPencil} className="icon text-blue" />Scrummaster Check-in</Link>
+            <Link to={`/groups/${groupId}/edit?date=${date}`} className="btn border m-auto mx-2"><FontAwesomeIcon
+                icon={faPencil} className="icon text-blue"/>Scrummaster Check-in</Link>
         </div>
         {isModalOpen && (
-            <CheckpointModal onClose={() => setIsModalOpen(false)} />
+            <CheckpointModal onClose={() => setIsModalOpen(false)}/>
         )}
     </div>
 }
 
-function CheckpointModal({ onClose }: { onClose: () => void }) {
+function CheckpointModal({onClose, onConfirm}:
+     {
+         onClose: () => void,
+         onConfirm: (name: string) => void,
+     }) {
+    const [name, setName] = useState("");
+
     return (
-            <div className="flex fixed inset-0 backdrop-blur-lg justify-center items-center" onClick={() => onClose()}>
-                <div className="bg-bg_h rounded-lg p-6 w-96 border" onClick={(e) => e.stopPropagation()}>
-                    <h2 className="text-fg mb-4">New Checkpoint:</h2>
-                    <input type="text"
-                           placeholder="Name:"
-                           className="w-full p-3 rounded-lg mb-4 outline-none border"
-                           />
+        <div className="flex fixed inset-0 backdrop-blur-lg justify-center items-center" onClick={() => onClose()}>
+            <div className="bg-bg_h rounded-lg p-6 w-96 border" onClick={(e) => e.stopPropagation()}>
+                <h2 className="text-fg mb-4">New Checkpoint:</h2>
+                <input value={name} onChange={(e) => setName(e.target.value)} type="text"
+                       placeholder="Name:"
+                       className="w-full p-3 rounded-lg mb-4 outline-none border"
+                />
+                <div className="flex justify-between">
+                    <button onClick={() => onConfirm(name)} className="btn border">Confirm</button>
                     <button onClick={onClose} className="btn border">Close</button>
                 </div>
-
             </div>
+
+        </div>
     )
 }
 
