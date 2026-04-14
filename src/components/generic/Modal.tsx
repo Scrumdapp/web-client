67f537a41b1ec8165ff771277a00c86c64494244
Luc van Ogtrop <luc@vanogtrop-deruiter.nl>
2026-04-14 14:30:04 +0200

    Update src/components/generic/Modal.tsx
    Co-authored-by: Jeroen <Jeroen@vandegeest.eu>

--- src/components/generic/Modal.tsx
+++ src/components/generic/Modal.tsx
@@ -11,7 +11,7 @@ export default function Modal({ onClose, title, children, actions }: ModalProps)
 
     return (
         <div className="flex fixed inset-0 backdrop-blur-lg justify-center items-center" onClick={onClose}>
-            <div className="bg-bg_h rounded-lg p-6 w-96 border" onClick={(e) => e.stopPropagation()}>
+            <div className="card w-96" onClick={(e) => e.stopPropagation()}>
                 <h2 className="text-fg mb-4">{title}</h2>
                 {children}
                 <div className="flex gap-2 mt-4 justify-between">
