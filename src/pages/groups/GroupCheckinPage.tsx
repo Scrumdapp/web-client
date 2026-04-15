4ba500a6cf612f53b565a3d0992fc095c8cdbf92
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-04-15 12:49:11 +0200

    Create Checkpoint will create/load extra checkin component

--- src/pages/groups/GroupCheckinPage.tsx
+++ src/pages/groups/GroupCheckinPage.tsx
@@ -17,22 +17,28 @@ export function GroupCheckinPage() {
     const [ searchParams ] = useSearchParams();
     const date = searchParams.get("date") ?? toScrumdappDate(new Date())
 
-    const [checkpoints, setCheckpoints] = useState<string>([date]);
+    const [checkpoints, setCheckpoints] = useState<string[]>([date]);
 
+    const handleCreate = () => {
+        setCheckpoints(prev => [...prev, date]);
+        modal.accept();
+    }
     return (
         <div className="space-y-3 ">
         <div className="flex justify-between card w-7/10 h-20 bg-bg_h border rounded-lg p-2 items-center">
             <h2 className="px-2">Checkpoint {date}</h2>
             <button className="btn border" onClick={modal.open}><FontAwesomeIcon icon={faAdd} className="text-blue"/> Create Checkpoint</button>
         </div>
-        <Checkin groupId={group.id} date={date}/>
+            {checkpoints.map((checkpointDate, index) => (
+                <Checkin key={index} groupId={group.id} date={checkpointDate} />
+            ))}
             <Modal state={modal}>
                 <div className="space-y-5">
                 <ModalHeadText>New Checkpoint</ModalHeadText>
                 <input className="write-section w-full!"></input>
                 <ModalActionRow>
                     <ModalCancelButton />
-                    <button className="btn btn-secondary border" onClick={modal.accept}>Create</button>
+                    <button className="btn btn-secondary border" onClick={handleCreate}>Create</button>
                 </ModalActionRow>
                 </div>
             </Modal>
