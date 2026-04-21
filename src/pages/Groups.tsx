1b76d7568a865fad05fd1621e5336714d5bc8e48
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-04-21 11:19:00 +0200

    Logic for sending POST request

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -6,11 +6,16 @@ import ModalHeadText from "../components/generic/modal/components/ModalHeadText.
 import ModalActionRow from "../components/generic/modal/components/ModalActionRow.tsx";
 import ModalCancelButton from "../components/generic/modal/components/ModalCancelButton.tsx";
 import {useState} from "react";
+import {LoadScreen} from "../components/generic/LoadScreen.tsx";
+import {useApi} from "../js/hooks/api/useApi.ts";
+import {ScrumdappApi} from "../js/hooks/api/scrumdappApi.ts";
 
 export default function Groups() {
 
     const modal = useModalState();
     const [name, setName] = useState("")
+    const createGroup = useApi(ScrumdappApi.createGroup());
+
 
     return (
         <div className="app-container horizontal justify-between">
@@ -21,12 +26,24 @@ export default function Groups() {
             </button>
             <Modal state={modal}>
                 <ModalHeadText>New Group</ModalHeadText>
-                <form>
+                <form id="create-group-form"
+                      onSubmit={(e) => {
+                          e.preventDefault();
+
+                          if (createGroup.loading) return;
+
+                          createGroup
+                              .runCommand({ name })
+                              .then(() => {
+                                  modal.close();
+                              });
+                      }}>
                     <input
                         type="text"
                         placeholder="Group Name"
                         className="write-section mb-4 w-full!"
                         value={name}
+                        maxLength="80" // Ignore error, shit just works fine, don't worry
                         onChange={(e) => setName(e.target.value)}
                         required
                     />
@@ -34,11 +51,12 @@ export default function Groups() {
                 <ModalActionRow>
                     <ModalCancelButton/>
                     <button
-                        onClick={modal.close}
+                        type="submit"
+                        form="create-group-form"
                         disabled={!name}
                         className={`btn border ${!name ? "opacity-50 cursor-not-allowed!" : ""}`}
                     >
-                        Submit
+                        {createGroup.loading ? <LoadScreen /> : "Submit"}
                     </button>
                 </ModalActionRow>
             </Modal>
