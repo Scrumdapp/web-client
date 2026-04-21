e0b8b4934ead23dab40459395d69d6d006e509de
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-04-21 15:05:09 +0200

    Resized 404 image

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -5,8 +5,8 @@ export default function ErrorPage() {
     const navigate = useNavigate();
 
     return (
-        <div className="app-container vertical gap-4">
-            <img src="https://http.cat/images/404.jpg" alt="404 error, page not found" className="border rounded-2xl mx-auto" />
+        <div className="vertical gap-3">
+            <img src="https://http.cat/images/404.jpg" alt="404 error, page not found" className="border rounded-2xl mx-auto m-h-100 m-w-100 h-90" />
             <Link
                 to=".." className="mx-auto bg-bg! card btn text-xl"
                 onClick={(e) => {e.preventDefault();navigate(-1);}}>Go back
