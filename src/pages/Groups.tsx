27747c575500adf132424664e8462d02a0aaf9e7
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-05-12 14:47:05 +0200

    Fixed constant reloading

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -13,12 +13,13 @@ import {GroupTile} from "../components/groups/GroupTile.tsx";
 import {useApiComponent} from "../js/hooks/api/useApiComponent.tsx";
 import {Link} from "react-router-dom";
 
+const GetGroups = useApiComponent(ScrumdappApi.getGroups());
+
 export default function Groups() {
 
     const modal = useModalState();
     const [name, setName] = useState("")
     const createGroup = useApi(ScrumdappApi.createGroup());
-    const GetGroups = useApiComponent(ScrumdappApi.getGroups());
     const [search, setSearch] = useState("");
 
     return (
