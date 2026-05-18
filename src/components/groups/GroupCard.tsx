506ba93f044e882013807d5b133f76d052619c0d
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-05-18 13:20:31 +0200

    Remove uneccesary div and reformat file

--- src/components/groups/GroupCard.tsx
+++ src/components/groups/GroupCard.tsx
@@ -1,22 +1,20 @@
-import {Group} from "../../js/models/group.ts";
+import { Group } from "../../js/models/group.ts";
 
 interface GroupCardProps {
-    group: Group;
+  group: Group;
 }
 
 
 export function GroupCard({ group }: GroupCardProps) {
 
-    return (
-            <div className="card hover:bg-bg2!">
-                <div>
-                    <p className="text-xs lg:text-2xl pb-2">{group.name}</p>
-                    <img
-                        src="https://http.cat/images/404.jpg"
-                        alt="Group Image"
-                        className="object-cover h-full rounded overflow-hidden"
-                    />
-            </div>
-        </div>
-    );
-        }
+  return (
+    <div className="card hover:bg-bg2!">
+      <p className="text-xs lg:text-2xl pb-2">{group.name}</p>
+      <img
+        src="https://http.cat/images/404.jpg"
+        alt="Group Image"
+        className="object-cover h-full rounded overflow-hidden"
+      />
+    </div >
+  );
+}
