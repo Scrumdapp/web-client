ead3fa39b98881f71bca8a6142f984d6b7dc937b
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-05-18 12:17:21 +0200

    Update src/components/groups/GroupTile.tsx
    Co-authored-by: Jeroen <Jeroen@vandegeest.eu>

--- src/components/groups/GroupTile.tsx
+++ src/components/groups/GroupTile.tsx
@@ -13,7 +13,7 @@ export function GroupTile({ group }: GroupTileProps) {
                     <p className="text-xs lg:text-2xl pb-2">{group.name}</p>
                     <img
                         src="https://http.cat/images/404.jpg"
-                        alt={"Group Image"}
+                        alt="Group Image"
                         className="object-cover h-full rounded overflow-hidden"
                     />
             </div>
