ffb2599abf84583b0063c71977028c66a133b79f
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-05-21 15:48:05 +0200

    Removed Logout.tsx

--- src/pages/Logout.tsx
+++
@@ -1,9 +0,0 @@
-import {LogoutButton} from "../components/generic/LogoutButton.tsx";
-
-export default function Logout() {
-    return(
-        <div>
-            <LogoutButton />
-        </div>
-    )
-}
