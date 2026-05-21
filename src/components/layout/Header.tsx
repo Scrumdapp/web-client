3ded99966b8bc8d879b258c6cc346bbceacdab96
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-05-21 14:55:37 +0200

    Fixed mx-2 on layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,34 +10,33 @@ export default function Header() {
     ];
 
         return (
-        <nav className="bg-bg_h mt-4 mb-7 flex px-1.75 rounded-2xl border shadow-xl w-full justify-between items-center z-50">
+            <header className="flex mx-2">
+            <nav className="bg-bg_h mt-4 mb-7 flex px-1.75 rounded-2xl border shadow-xl w-full justify-between items-center z-50">
+                        <div className="shrink-0 text-fg text-2xl font-bold max-w-20">
+                            <Link to="/public">Scrumdapp</Link>
+                        </div>
 
-
-                    <div className="shrink-0 text-fg text-2xl font-bold max-w-20">
-                        <Link to="/public">Scrumdapp</Link>
-                    </div>
-
-                    <div className="hidden md:flex space-x-6 text-black font-medium">
-                        {links.map((link, i) => (
-                            <Link key={i} to={link.to} className="btn w-20">
-                                {link.label}
-                            </Link>
-                        ))}
-                    </div>
-                    <div className="dropdown">
-                        <p className="text-right">UserName123</p>
-                        <div className="dropdown-content">
-                            <div className="p-5">
-                                <Link to="/login" className="btn-mobile">Profile</Link>
-                                <a href="/public" className="btn-mobile">Home</a>
-                                <a href="/about" className="btn-mobile">About</a>
-                                <a href="/privacy" className="btn-mobile">Privacy</a>
-                                <a href="/groups" className="btn-mobile">Groups</a>
-                                <a href="/support" className="btn-mobile">Support</a>
-                                <LogoutButton />
+                        <div className="hidden md:flex space-x-6 text-black font-medium">
+                            {links.map((link, i) => (
+                                <Link key={i} to={link.to} className="btn w-20">
+                                    {link.label}
+                                </Link>
+                            ))}
+                        </div>
+                        <div className="dropdown">
+                            <p className="text-right">UserName123</p>
+                            <div className="dropdown-content">
+                                <div className="p-5">
+                                    <Link to="/login" className="btn-mobile">Profile</Link>
+                                    <a href="/public" className="btn-mobile">Home</a>
+                                    <a href="/about" className="btn-mobile">About</a>
+                                    <a href="/privacy" className="btn-mobile">Privacy</a>
+                                    <a href="/groups" className="btn-mobile">Groups</a>
+                                    <a href="/support" className="btn-mobile">Support</a>
+                                    <LogoutButton />
+                                </div>
                             </div>
                         </div>
-                    </div>
 
 
 
@@ -45,6 +44,7 @@ export default function Header() {
 
 
 
-        </nav>
+            </nav>
+            </header>
         )
     }
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer.tsx";
 import Header from "./Header.tsx";
 
 export default function Layout({ children }: PropsWithChildren) {    return (
-        <div className="min-h-screen flex flex-col mx-2">
+        <div className="min-h-screen flex flex-col overflow-x-hidden">
             <Header />
             <main className="flex-1 flex flex-col">{children}</main>
             <Footer />
