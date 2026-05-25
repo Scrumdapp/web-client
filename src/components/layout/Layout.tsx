6e2bbad2aedaa9655001166d34b409b495ef5591
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-05-25 20:17:21 +0200

    remove unneeded overflow in layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer.tsx";
 import Header from "./Header.tsx";
 
 export default function Layout({ children }: PropsWithChildren) {    return (
-        <div className="min-h-screen flex flex-col overflow-x-hidden">
+        <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1 flex flex-col">{children}</main>
             <Footer />
