9b848bbb8b78b4ad2d54c54a251fc36c180afe48
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-05-29 10:12:51 +0200

    Add invite routes

---
+++ src/js/hooks/api/routes/inviteRoutes.tsx
@@ -0,0 +1,47 @@
+import { createProcessor, makeApiRequest } from "../apiUtils.ts";
+import { InviteResponse } from "../../../models/invites.tsx";
+
+export function CreateInvite() {
+    return createProcessor("createInvite", (groupId: number, expiresAt: Date, password: string) => {
+        return makeApiRequest<InviteResponse>("POST", "/invites", {
+            query: { group: groupId },
+            body: {
+                password,
+                expiresAt
+            }
+        })
+    })
+}
+
+export function GetGroupInvites() {
+    return createProcessor("getGroupInvites", (groupId: number) => {
+        return makeApiRequest<InviteResponse>("GET", "/invites", {
+            query: { group: groupId },
+        })
+    })
+}
+
+export function GetGroupInvite() {
+    return createProcessor("getGroupInvite", (inviteId: number) => {
+        return makeApiRequest<InviteResponse>("GET", "/invites/{inviteId}", {
+            params: { inviteId }
+        })
+    })
+}
+
+export function DeleteInvite() {
+    return createProcessor("deleteInvite", (inviteId: number) => {
+        return makeApiRequest("DELETE", "/invites/{inviteId}", {
+            params: { inviteId }
+        })
+    })
+}
+
+export function AcceptInvite() {
+    return createProcessor("createInvite", (inviteId: number, userId: number, token: number, password: string) => {
+        return makeApiRequest("POST", "/invites/{inviteId}/accept", {
+            params: { inviteId },
+            body: { userId, token, password }
+        })
+    })
+}
