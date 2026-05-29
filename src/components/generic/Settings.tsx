0c0c87848b56d04b103c843effa3f6a01b0c256d
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-05-29 14:47:35 +0200

    Have set up the routing for invite cycle

---
+++ src/components/generic/Settings.tsx
@@ -0,0 +1,14 @@
+export default function Settings() {
+    return (
+        <main>
+            <div>
+                ...All Invites
+            </div>
+            <div>
+                <button className="btn btn-secondary border">
+                    Create Invite
+                </button>
+            </div>
+        </main>
+    )
+}
--- src/router/GroupRouter.tsx
+++ src/router/GroupRouter.tsx
@@ -3,6 +3,7 @@ import {ErrorScreen} from "../components/generic/ErrorScreen.tsx";
 import {ApiError} from "../js/hooks/api/apiError.ts";
 import {GroupCheckpointPage} from "../pages/groups/GroupCheckpointPage.tsx";
 import InviteUser from "../components/groups/InviteUser.tsx";
+import Settings from "../components/generic/Settings.tsx";
 
 export function GroupRouter() {
 
@@ -10,7 +11,8 @@ export function GroupRouter() {
         <Routes>
             <Route index element={<GroupCheckpointPage/>} />
             <Route path="*" element={<ErrorScreen error={new ApiError(404, "Page Not Found")} />} />
-            <Route path="/settings" element={<InviteUser/>} />
+            <Route path="/settings" element={<Settings/>} />
+            <Route path="/settings/invite" element={<InviteUser/>} />
         </Routes>
     )
 }
