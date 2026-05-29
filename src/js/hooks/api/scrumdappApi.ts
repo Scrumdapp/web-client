e9ac4b2478f3d4a0218c5f0747340d30421ef0d9
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-05-29 10:49:35 +0200

    Add InviteRoutes to ScrumdappAPI

--- src/js/hooks/api/scrumdappApi.ts
+++ src/js/hooks/api/scrumdappApi.ts
@@ -3,11 +3,13 @@ import * as GroupRoutes from "./routes/groupRoutes.tsx"
 import * as GroupUserRoutes from "./routes/groupUserRoutes.tsx"
 import * as GroupSessionRoutes from "./routes/groupSessions.tsx"
 import * as GroupCheckpointRoutes from "./routes/groupCheckpointRoutes.tsx"
+import * as InviteRoutes from "./routes/inviteRoutes.tsx"
 
 export const ScrumdappApi = {
     ...UserRoutes,
     ...GroupRoutes,
     ...GroupUserRoutes,
     ...GroupSessionRoutes,
-    ...GroupCheckpointRoutes
+    ...GroupCheckpointRoutes,
+    ...InviteRoutes
 }
