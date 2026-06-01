026e3a9ea53aac1167eb4e617ef1d7a6016731d6
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-06-01 10:20:58 +0200

    register handler

--- msw/worker.ts
+++ msw/worker.ts
@@ -1,10 +1,11 @@
-import {setupWorker} from "msw/browser";
-import {userHandlers} from "./handlers/userHandlers.ts";
-import {groupUserHandler} from "./handlers/groupUserHandler.ts";
-import {groupHandlers} from "./handlers/groupHandlers.ts";
-import {groupCheckinsHandlers} from "./handlers/groupCheckinsHandlers.ts";
-import {groupCheckpointHandlers} from "./handlers/groupCheckpointHandlers.ts";
-import {userCookieHandler} from "./handlers/userCookieHandler.ts";
+import { setupWorker } from "msw/browser";
+import { userHandlers } from "./handlers/userHandlers.ts";
+import { groupUserHandler } from "./handlers/groupUserHandler.ts";
+import { groupHandlers } from "./handlers/groupHandlers.ts";
+import { groupCheckinsHandlers } from "./handlers/groupCheckinsHandlers.ts";
+import { groupCheckpointHandlers } from "./handlers/groupCheckpointHandlers.ts";
+import { userCookieHandler } from "./handlers/userCookieHandler.ts";
+import { trendsHandlers } from "./handlers/trendsHandlers.ts";
 
 export const worker = setupWorker(
     ...userHandlers,
@@ -13,4 +14,5 @@ export const worker = setupWorker(
     ...groupCheckinsHandlers,
     ...groupCheckpointHandlers,
     ...userCookieHandler,
+    ...trendsHandlers
 )
