a40ab9b5136dd2d9164be0b2f9ac72786c1378e2
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-06-01 10:31:53 +0200

    Link invite to settings

--- src/components/generic/Settings.tsx
+++ src/components/generic/Settings.tsx
@@ -1,13 +1,17 @@
+import {Link} from "react-router-dom";
+
 export default function Settings() {
     return (
         <main>
-            <div>
-                ...All Invites
-            </div>
-            <div>
-                <button className="btn btn-secondary border">
-                    Create Invite
-                </button>
+            <div className="flex flex-col border">
+                <div>
+                    <Link to={'invite'} className="flex float-right btn btn-secondary border">
+                        Create Invite
+                    </Link>
+                </div>
+                <div>
+                    ...All Invites
+                </div>
             </div>
         </main>
     )
