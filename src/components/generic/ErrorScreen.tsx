d0b59e4142183c54772637ac4cf87227dcc5c85c
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-06-08 17:49:43 +0200

    Add error for no trends

--- src/components/generic/ErrorScreen.tsx
+++ src/components/generic/ErrorScreen.tsx
@@ -1,9 +1,13 @@
-import type {ApiError} from "../../js/hooks/api/apiError.ts";
+import type { ApiError } from "../../js/hooks/api/apiError.ts";
 
-export function ErrorScreen({error}: {error: ApiError}) {
+export function ErrorScreen({ error }: { error: ApiError }) {
     return (
         <div className="center fade-in">
-            <h2>Error: <strong>{error.status}</strong></h2>
+            {error.status == 999 ? (
+                <h2>Error!</h2>
+            ) : (
+                <h2>Error: <strong>{error.status}</strong></h2>
+            )}
             <p>{error.message}</p>
             <p>{error.detail}</p>
         </div>
