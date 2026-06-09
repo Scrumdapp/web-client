15526db397865c4768c74cb8af35ea355286f3de
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-06-09 15:58:22 +0200

    Added `mb-2`

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
     ];
 
     return (
-    <footer className="px-2 flex-col">
+    <footer className="px-2 mb-2 flex-col">
         <div className="mx-auto p-3 rounded-2xl bg-bg_h shadow-xl border w-full">
             <div className="flex flex-row items-center justify-between px-3">
                 <span>© 2026 Scrumdapp | All rights reserved</span>
