794b9dfe0a602aca208303ba99835f448de6688b
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-06-10 13:30:55 +0200

    Fix styling to sidebar link and add key

--- src/components/groups/GroupSidebarDates.tsx
+++ src/components/groups/GroupSidebarDates.tsx
@@ -10,9 +10,11 @@ export function GroupSidebarDates() {
 
     return (
         <div className="card vertical gap-1 text-center">
+            <span>Recent</span>
+            <hr className="text-gray my-2" />
             <GetDatesApiComponent input={[group.id, 5]}>
                 {(dates) => dates.dates.map((date) => (
-                    <Link to={`/groups/${group.id}?date=${date.date}`} className="btn">
+                    <Link key={date.date} to={`/groups/${group.id}?date=${date.date}`} className="btn text-gray! py-1!">
                         {date.date}
                     </Link>
                 ))}
