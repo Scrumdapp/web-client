b43630cbf589ad3d15dc0c8c759be4d9bda946b9
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-06-10 13:22:37 +0200

    Add links

--- src/components/groups/GroupSidebarDates.tsx
+++ src/components/groups/GroupSidebarDates.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import { useGroup } from "../../js/context/group/useGroup"
+import { ScrumdappApi } from "../../js/hooks/api/scrumdappApi"
+import { useApiComponent } from "../../js/hooks/api/useApiComponent"
+
 export function GroupSidebarDates() {
+
+    const group = useGroup();
+    const GetDatesApiComponent = useApiComponent(ScrumdappApi.getRecentCheckpointDates())
 
     return (
         <div className="card vertical gap-1 text-center">
+            <GetDatesApiComponent input={[group.id, 5]}>
+                {(dates) => dates.dates.map((date) => (
+                    <Link to={`/groups/${group.id}?date=${date.date}`} className="btn">
+                        {date.date}
+                    </Link>
+                ))}
+            </GetDatesApiComponent >
         </div>
     )
 }
