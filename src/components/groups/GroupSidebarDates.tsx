6b1ee4308a1dadad7f8bce39ebff9c6ab475e2fd
Luc-v-O <luc@vanogtrop-deruiter.nl>
2026-06-10 12:56:28 +0200

    Removed unused element

--- src/components/groups/GroupSidebarDates.tsx
+++ src/components/groups/GroupSidebarDates.tsx
@@ -2,8 +2,6 @@ export function GroupSidebarDates() {
 
     return (
         <div className="card vertical gap-1 text-center">
-            Something
-            <hr className="text-gray"/>
         </div>
     )
 }
