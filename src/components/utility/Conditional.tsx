83bf7aa86f8c8507707e816828974f3cb0854542
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-06-10 10:55:17 +0200

    Add conditional components

---
+++ src/components/utility/Conditional.tsx
@@ -0,0 +1,10 @@
+import { memo, ReactNode } from "react";
+
+
+export const HideIf = memo(({ condition, children }: { condition: boolean, children: ReactNode }) => {
+    return condition ? null : children
+})
+
+export const ShowIf = memo(({ condition, children }: { condition: boolean, children: ReactNode }) => {
+    return condition ? null : children
+})
