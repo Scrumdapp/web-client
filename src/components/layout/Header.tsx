caf4b4dcc1f2754817662f2edebd139e5277b9c4
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-06-12 14:28:54 +0200

    Make header scroll with the user

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,9 +14,9 @@ export default function Header() {
         { to: "/groups", label: "Groups" },
     ];
 
-    return (
-        <header className="flex mx-2">
-            <nav className="bg-bg_h mt-2 mb-7 flex px-1 rounded-full border shadow-xl w-full justify-between items-center z-50">
+    return (<>
+        <header className="flex mx-2 fixed top-0 left-0 right-0">
+            <nav className="bg-bg_h mt-2 flex px-1 rounded-full border shadow-xl w-full justify-between items-center z-50">
                 <div className="flex-1 text-fg text-2xl font-bold">
                     <Link to="/groups" className="horizontal gap-1 items-center">
                         <img alt="scrumdapp-logo" src={ScrumdappLogoTransparant} className="nav-icon" />
@@ -48,5 +48,6 @@ export default function Header() {
                 </div>
             </nav>
         </header>
-    )
+        <div className="h-20" />
+    </>)
 }
