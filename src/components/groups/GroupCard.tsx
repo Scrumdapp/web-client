aa94697518cb784d19797e3e512f8540099faa0d
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-06-18 15:48:45 +0200

    Added user icon and placed the amount of user beside group name

--- src/components/groups/GroupCard.tsx
+++ src/components/groups/GroupCard.tsx
@@ -1,6 +1,8 @@
 import { PartialGroup } from "../../js/models/group.ts";
 import {useApiComponent} from "../../js/hooks/api/useApiComponent.tsx";
 import {ScrumdappApi} from "../../js/hooks/api/scrumdappApi.ts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
 
 interface GroupCardProps {
     group: PartialGroup;
@@ -11,12 +13,14 @@ export function GroupCard({ group }: GroupCardProps) {
 
     return (
         <div className="card hover:bg-bg2!">
-            <GroupUsers input={[group.id]}>
-                {(users) => (
-                    <p>{users.length} Users</p>
-                )}
-            </GroupUsers>
-            <p className="text-lg pb-2 overflow-hidden whitespace-nowrap text-ellipsis max-w-60ch">{group.name}</p>
+            <div className="flex flex-row justify-between">
+                <p className="text-lg pb-2 overflow-hidden whitespace-nowrap text-ellipsis max-w-60ch">{group.name}</p>
+                <GroupUsers input={[group.id]}>
+                    {(users) => (
+                        <p>{users.length}<FontAwesomeIcon icon={faUser} className="text-blue"/></p>
+                    )}
+                </GroupUsers>
+                </div>
             <img
                 src="https://http.cat/images/404.jpg"
                 alt="Group Image"
