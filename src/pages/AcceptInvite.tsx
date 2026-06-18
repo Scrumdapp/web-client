b1aa75ac60c8c9bb48f61392798799e60e647f23
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-06-18 09:26:41 +0200

    seperated component responsibility

--- src/pages/AcceptInvite.tsx
+++ src/pages/AcceptInvite.tsx
@@ -1,8 +1,9 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import { ScrumdappApi } from "../js/hooks/api/scrumdappApi.ts";
 import {useApi} from "../js/hooks/api/useApi.ts";
 import {LoadScreen} from "../components/generic/LoadScreen.tsx";
+import useTempValue from "../js/hooks/useTempValue.ts";
 
 export default function AcceptInvite() {
     const params = useParams()
@@ -11,55 +12,88 @@ export default function AcceptInvite() {
     const [searchParams] = useSearchParams()
     const token = searchParams.get("token")
 
-    const navigate = useNavigate()
-
-    const [password, setPassword] = useState("")
-
     const getInvite = useApi(ScrumdappApi.GetGroupInvite())
-    const acceptInvite = useApi(ScrumdappApi.AcceptInvite())
-
 
     useEffect(() => {
         getInvite.runCommand(inviteId, token ?? "")
-    }, [getInvite.runCommand])
+    }, [getInvite.runCommand, inviteId, token])
 
-    function handleJoinInvite() {
-        acceptInvite.runCommand(inviteId, token ?? "", password)
-            .then((data) => {
-                const groupId = data.groupId
-                navigate(`/groups${groupId ? `/${groupId}` : ''}`)
-            })
+    if (token == null) {
+        return <p>Expected a token</p>
     }
-
-    if (getInvite.loading) return <LoadScreen />
-    if (getInvite.error) {
-        return <p>{getInvite.error?.message ?? "Something went wrong retrieving invite"}</p>
-    }
-
-    if (acceptInvite.error) {
-        return <p>{acceptInvite.error?.message ?? "Could not accept invite"}</p>
+    if (getInvite.loading) {
+        return <LoadScreen />
+    } else if (getInvite.error) {
+        return (
+            <div className="app-container">
+                <div className="card flex flex-col">
+                    <p>{getInvite.error?.message ?? "Something went wrong retrieving invite"}</p>
+                </div>
+            </div>
+        )
     }
 
     return (
         <div className="app-container">
             <div className="card flex flex-col">
-                <h1>You're invited to {}!</h1>
-                <p>Enter your given password underneath.</p>
-                <div>
-                    <div className="py-3">
-                        <input className="write-section w-full!"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <button onClick={handleJoinInvite} className="btn btn-secondary border flex float-right">
-                            Join
-                        </button>
-                    </div>
-                </div>
+                <h1>You have received an invite</h1>
+                <InviteForm inviteId={inviteId} token={token} />
             </div>
         </div>
+    )
+}
 
+function InviteForm({inviteId, token}: {
+    inviteId: number,
+    token: string
+}) {
+
+    const navigate = useNavigate()
+    const errorMessage = useTempValue<string>()
+    const [password, setPassword] = useState("")
+
+    const acceptInvite = useApi(ScrumdappApi.AcceptInvite())
+
+    function handleSubmit(event: { preventDefault: () => void; }) {
+        event.preventDefault()
+
+        if (!password.trim()) {
+            return
+        }
+
+        acceptInvite.runCommand(inviteId, token ?? "", password)
+            .then((data) => {
+                const groupId = data.groupId
+                navigate(`/groups${groupId ? `/${groupId}` : ''}`)
+            })
+            .catch((error) => {
+                //TODO (filter errors)
+                errorMessage.showContent("Something went wrong, please try again later", 40000)
+            })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Password
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    required
+                    className="write-section w-full!"
+                />
+            </label>
+            <div className="flex items-center w-full my-3">
+                {errorMessage.visibility && (
+                    <p className="text-red-dim">{errorMessage.content}</p>
+                )}
+                <button
+                    type="submit"
+                    className="btn btn-secondary ml-auto border">
+                    Submit
+                </button>
+            </div>
+        </form>
     )
 }
