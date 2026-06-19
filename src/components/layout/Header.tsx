1b563a86e2e8c5843571e558c44f47cc6d83ca63
ThomasHU612 <thomas.middelbos@student.hu.nl>
2026-06-19 11:08:28 +0200

    you can now use keyboard for dropdown in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -31,11 +31,11 @@ export default function Header() {
                         </Link>
                     ))}
                 </div>
-                <div className="flex-1 nav-dropdown cursor-default">
-                    <div className="horizontal justify-end items-center gap-1">
+            <div className="flex-1 nav-dropdown cursor-default">
+                <button className="horizontal float-right items-center gap-1">
                         <span>{`${user.first_name} ${user.last_name}`}</span>
                         <img alt="user-profile-picture" src={user.avatar ?? "/Scrumdaddy.png"} className="nav-icon" />
-                    </div>
+                    </button>
                     <div className="nav-dropdown-content -right-[0.5px]!">
                         <div className="py-4 px-1.5 text-left">
                             <Link to="/settings" className="btn-nav-dropdown hover:text-yellow">
