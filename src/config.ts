aa152619e777b0e6fc04227ded01510462fcfa69
Jeroen <Jeroen@vandegeest.eu>
2026-09-04 13:35:03 +0200

    change discord url to keycloak login url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 
 export namespace WebConfig {
-    export const discordLoginUrl: string = import.meta.env.VITE_DISCORD_LOGIN_URL ?? "/api/oauth2/authorization/discord"
+    export const discordLoginUrl: string = import.meta.env.VITE_DISCORD_LOGIN_URL ?? "/api/oauth2/authorization/keycloak"
     export const logoutUrl: string = import.meta.env.VITE_LOGOUT_URL ?? "/api/oauth2/logout"
 }
 
