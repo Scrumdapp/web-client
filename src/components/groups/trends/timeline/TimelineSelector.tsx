c01370c6cccf2aed31ee6864b493a71fd326ffd1
jasperth2 <jasperth2@gmail.com>
2026-09-10 12:30:46 +0200

    fix/menu-open-hides-scrollbar

--- src/components/groups/trends/timeline/TimelineSelector.tsx
+++ src/components/groups/trends/timeline/TimelineSelector.tsx
@@ -1,63 +1,68 @@
-import { Menu, MenuItems, MenuItem, MenuButton } from "@headlessui/react"
-import { useEffect, useState } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
-import { GroupTimelineDisplayType } from "./GroupTimelineTrends"
+import { Menu, MenuItems, MenuItem, MenuButton } from "@headlessui/react";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { GroupTimelineDisplayType } from "./GroupTimelineTrends";
 import { useTranslation } from "react-i18next";
 
+export function TimelineSelector({
+  onRangeSelected = () => {},
+  defaultOption = 0,
+}: {
+  onRangeSelected?: (type: GroupTimelineDisplayType) => void;
+  defaultOption?: number;
+}) {
+  const { t } = useTranslation();
 
-export function TimelineSelector(
+  const options = [
     {
-        onRangeSelected = () => { },
-        defaultOption = 0
-    }: {
-        onRangeSelected?: (type: GroupTimelineDisplayType) => void,
-        defaultOption?: number
-    }) {
-    
-    const {t} = useTranslation();
+      name: t("trends.periodic"),
+      value: GroupTimelineDisplayType.Periodic,
+    },
+    {
+      name: t("trends.cumulative"),
+      value: GroupTimelineDisplayType.Cumulative,
+    },
+  ];
 
-    const options = [
-        {
-            name: t("trends.periodic"),
-            value: GroupTimelineDisplayType.Periodic,
-        },
-        {
-            name: t("trends.cumulative"),
-            value: GroupTimelineDisplayType.Cumulative,
-        },
-    ]
+  const [selectedOption, setSelectedOption] = useState(defaultOption);
 
-    const [selectedOption, setSelectedOption] = useState(defaultOption)
-
-    const selectOption = (index: number) => {
-        setSelectedOption(index)
-        if (onRangeSelected) {
-            const option = options[index]
-            onRangeSelected(option.value)
-        }
+  const selectOption = (index: number) => {
+    setSelectedOption(index);
+    if (onRangeSelected) {
+      const option = options[index];
+      onRangeSelected(option.value);
     }
+  };
 
-    useEffect(() => {
-        setTimeout(() => { selectOption(defaultOption ?? 0) }, 1)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      selectOption(defaultOption ?? 0);
+    }, 1);
+  }, []);
 
-    return (
-        <Menu as="div" className="relative">
-            <MenuButton
-                className="btn border min-w-32"
-            >
-                {options[selectedOption].name}
-                <FontAwesomeIcon icon={faChevronDown} className={`shrink-0`} />
-            </MenuButton>
-            <MenuItems transition className="absolute z-10 mt-2 border rounded-md bg-bg w-full">
-                {options.map((it, i) => (
-                    <MenuItem key={i} as="button" className="btn w-full" onClick={() => selectOption(i)}>
-                        {it.name}
-                    </MenuItem>
-                ))}
-            </MenuItems>
-        </Menu>
-    )
-
+  return (
+    <Menu as="div" className="relative">
+      <MenuButton className="btn border min-w-32">
+        {options[selectedOption].name}
+        <FontAwesomeIcon icon={faChevronDown} className={`shrink-0`} />
+      </MenuButton>
+      <MenuItems
+        modal={false}
+        transition
+        className="absolute z-10 mt-2 border rounded-md bg-bg w-full"
+      >
+        {options.map((it, i) => (
+          <MenuItem
+            key={i}
+            as="button"
+            className="btn w-full"
+            onClick={() => selectOption(i)}
+          >
+            {it.name}
+          </MenuItem>
+        ))}
+      </MenuItems>
+    </Menu>
+  );
 }
