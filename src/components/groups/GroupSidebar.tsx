e410e7b3bda820e21411dc7747a5b28479eb05c8
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-09-10 12:33:32 +0200

    Hide group icon

--- src/components/groups/GroupSidebar.tsx
+++ src/components/groups/GroupSidebar.tsx
@@ -4,29 +4,36 @@ import { useGroup } from "../../js/context/group/useGroup.ts";
 import { Link } from "react-router"
 import { faChartSimple, faGear, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
+import { ShowIf } from "../utility/Conditional.tsx";
+import { useUser } from "../../js/context/user/useUser.ts";
+import { hasRole, Role } from "../../js/utils/userPermissions.ts";
 
 export function GroupSidebar() {
     const { t } = useTranslation();
-    const group = useGroup()
-
-    const links = [
-        { path: "calendar", icon: faCalendarDays, color: "text-green", text: t("checkpoint.sidebar.calendar") },
-        { path: "trends", icon: faChartSimple, color: "text-yellow", text: t("checkpoint.sidebar.trends") },
-        { path: "settings", icon: faGear, color: "text-red", text: t("checkpoint.sidebar.settings") },
-    ]
+    const group = useGroup();
+    const user = useUser();
 
     return (
         <div className="card vertical gap-1">
             <Link to={`/groups/${group.id}`} className="btn justify-start!">
-                <FontAwesomeIcon icon={faHouse} className="text-blue"/> {t("checkpoint.sidebar.today")}
+                <FontAwesomeIcon icon={faHouse} className="text-blue" /> {t("checkpoint.sidebar.today")}
             </Link>
             <hr className="text-gray" />
-            {links.map((link, i) => (
-                <Link key={i} to={`/groups/${group.id}/${link.path}`} className="btn justify-start!">
-                    <FontAwesomeIcon icon={link.icon} className={link.color} />
-                    {link.text}
+
+            <Link to={`/groups/${group.id}/calendar`} className="btn">
+                <FontAwesomeIcon icon={faCalendarDays} className="text-green" />
+                {t("checkpoint.sidebar.calendar")}
+            </Link>
+            <Link to={`/groups/${group.id}/trends`} className="btn">
+                <FontAwesomeIcon icon={faChartSimple} className="text-yellow" />
+                {t("checkpoint.sidebar.trends")}
+            </Link>
+            <ShowIf condition={hasRole(user, Role.Coach)}>
+                <Link to={`/groups/${group.id}/settings`} className="btn">
+                    <FontAwesomeIcon icon={faGear} className="text-red" />
+                    {t("checkpoint.sidebar.settings")}
                 </Link>
-            ))}
+            </ShowIf>
         </div>
     )
 }
