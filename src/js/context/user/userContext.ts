cc43aa5f4111c4e4468b082c9d7d190a4f021783
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-09-15 11:14:57 +0200

    Add notify function to userContext

--- src/js/context/user/userContext.ts
+++ src/js/context/user/userContext.ts
@@ -1,8 +1,14 @@
-import {createContext} from "react";
-import type {User} from "../../models/user.ts";
+import { createContext } from "react";
+import type { User } from "../../models/user.ts";
 
 export class UserContextState {
     user?: User
+    loggedIn?: boolean
+    notifyLoggedOut: () => void
+
+    constructor(notifyLoggedOut: () => void) {
+        this.notifyLoggedOut = notifyLoggedOut
+    }
 }
 
 export const userContext = createContext<UserContextState | null>(null)
