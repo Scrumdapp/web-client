ee04cd70eaa3adb1f5a712af1e22009a3ad9e952
JasperTHU <jasper.terhennepe@student.hu.nl>
2026-09-15 10:34:20 +0200

    Same changes without prettier formatting

--- src/components/checkins/checkpointcomponents/AttendanceDropDownMenu.tsx
+++ src/components/checkins/checkpointcomponents/AttendanceDropDownMenu.tsx
@@ -1,72 +1,59 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { useEffect, useState } from "react";
-import {
-  attendanceOptions,
-  getAttendanceColorScrummaster,
-} from "../../../js/utils/colorUtils.ts";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useTranslation } from "react-i18next";
+import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/react";
+import {useEffect, useState} from "react";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useTranslation} from "react-i18next";
 
-type AttendanceDropDownMenuProps = {
-  value?: string | null;
-  onChange?: (value: string | null) => void;
+type TimeDurationDropdownMenuProps = {
+    value?: number | null;
+    onChange?: (value: number | null) => void;
 };
 
-export function AttendanceDropDownMenu({
-  value,
-  onChange,
-}: AttendanceDropDownMenuProps) {
-  const { t } = useTranslation();
-  const [localValue, setLocalValue] = useState<string | null>(value ?? null);
+export const TimeDuration = [
+    { labelKey: "invite.duration.twelvehours", hours: 12 },
+    { labelKey: "invite.duration.oneday", hours: 24 },
+    { labelKey: "invite.duration.twodays", hours: 48 },
+    { labelKey: "invite.duration.threedays", hours: 72 },
+    { labelKey: "invite.duration.oneweek", hours: 168 },
+];
 
-  const updateValue = (value: string | null) => {
-    setLocalValue(value ?? null);
-    if (onChange != null) {
-      onChange(value ?? null);
-    }
-  };
+export function TimeDurationDropdownMenu({ value, onChange }: TimeDurationDropdownMenuProps) {
+    const {t} = useTranslation();
+    const [localValue, setLocalValue] = useState<number | null>(value ?? null);
 
-  useEffect(() => {
-    setLocalValue(value ?? null);
-  }, [value]);
+    const updateValue = (value: number | null) => {
+        setLocalValue(value);
+        onChange?.(value);
+    };
 
-  const resolvedValue = value !== null ? (value ?? null) : localValue;
-  const currentOption =
-    attendanceOptions.find((opt) => opt.value === resolvedValue) ??
-    attendanceOptions[0];
-  const currentColor = getAttendanceColorScrummaster(resolvedValue);
+    useEffect(() => {
+        setLocalValue(value ?? null);
+    }, [value]);
 
-  return (
-    <Menu as="div" className="relative w-full w-[20%]">
-      <MenuButton className="btn-attendance border cursor-pointer">
-        <span className={`text-left ${currentColor}`}>
-          {t(currentOption.labelKey)}
-        </span>
-        <FontAwesomeIcon
-          icon={faChevronDown}
-          className={`${currentColor} shrink-0`}
-        />
-      </MenuButton>
-      <MenuItems
-        modal={false}
-        transition
-        className="absolute z-10 mt-2 border rounded-md bg-bg w-full"
-      >
-        <div>
-          {attendanceOptions.map((opt) => (
-            <MenuItem
-              key={opt.labelKey}
-              as="button"
-              type="button"
-              onClick={() => updateValue(opt.value)}
-              className={`btn-attendance-dropdown ${opt.color}`}
-            >
-              {t(opt.labelKey)}
-            </MenuItem>
-          ))}
-        </div>
-      </MenuItems>
-    </Menu>
-  );
+    const currentOption = TimeDuration.find((opt) => opt.hours === localValue) ?? TimeDuration[0];
+
+    return (
+        <Menu as="div" className="relative w-full">
+            <MenuButton className="btn-attendance border cursor-pointer h-10.5!">
+                <span className="text-left">
+                    {t(currentOption.labelKey)}
+                </span>
+                <FontAwesomeIcon icon={faChevronDown} className="shrink-0"/>
+            </MenuButton>
+            <MenuItems modal={false} transition className="absolute z-10 mt-2 border rounded-md bg-bg w-full py-1">
+                <div>
+                    {TimeDuration.map(opt => (
+                        <MenuItem
+                            key={t(opt.labelKey)}
+                            as="button"
+                            type="button"
+                            onClick={() => updateValue(opt.hours)}
+                            className="btn-attendance-dropdown py-1">
+                            {t(opt.labelKey)}
+                        </MenuItem>
+                    ))}
+                </div>
+            </MenuItems>
+        </Menu>
+    )
 }
