20820972ba0cb8557aa4f286e1f77129acff57b3
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-09-24 10:52:57 +0200

    fix esling config

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,6 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-// import prettier from "eslint-config-prettier"
-import prettier from "eslint-plugin-prettier"
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -16,7 +14,6 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
