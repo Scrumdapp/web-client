734997d8a5a476dcdbbbb393b39798ffd691db4c
Jeroen van de Geest <jeroen@vandegeest.eu>
2026-09-24 11:34:10 +0200

    remove checkin handler

--- msw/worker.ts
+++ msw/worker.ts
@@ -2,7 +2,6 @@ import { setupWorker } from "msw/browser";
 import { userHandlers } from "./handlers/userHandlers.ts";
 import { groupUserHandler } from "./handlers/groupUserHandler.ts";
 import { groupHandlers } from "./handlers/groupHandlers.ts";
-import { groupCheckinsHandlers } from "./handlers/groupCheckinsHandlers.ts";
 import { groupCheckpointHandlers } from "./handlers/groupCheckpointHandlers.ts";
 import { userCookieHandler } from "./handlers/userCookieHandler.ts";
 import { inviteHandlers } from "./handlers/inviteHandlers.ts";
